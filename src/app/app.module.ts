4a0b123b23bd4b30ebdb12eee8d1235fc0882989
diox <markuswagner93@gmail.com>
2018-06-28 22:30:33 +0200

    added pages and new scroll ability, also init for redux

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppComponent} from './app.component';
-import {ScrollToModule} from '@nicky-lenaers/ngx-scroll-to';
-import {NavigationComponent} from './components/navigation/navigation.component';
-import {HomeComponent} from './pages/home/home.component';
+import { AppComponent } from './app.component';
+import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PicturesComponent } from './pages/pictures/pictures.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    HomeComponent
+    HomeComponent,
+    PicturesComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
