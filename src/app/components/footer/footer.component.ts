41d0e2e38e15d47f8a9607d9c651ef53cdd5e16c
diox <markuswagner93@gmail.com>
2018-07-25 23:23:12 +0200

    started to implement footer, added contact details

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  socialVisible = false;
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleSocials(event) {
+    event.stopPropagation();
+    this.socialVisible = !this.socialVisible;
+    const clickListener = () => {
+      this.socialVisible = false;
+    };
+
+    if (this.socialVisible) {
+      document.body.addEventListener('click', clickListener, false);
+    } else {
+      document.body.removeEventListener('click', clickListener, false);
+    }
+  }
+
+  openSocial(url) {
+    this.socialVisible = false;
+    window.open(url);
+  }
 }
+
